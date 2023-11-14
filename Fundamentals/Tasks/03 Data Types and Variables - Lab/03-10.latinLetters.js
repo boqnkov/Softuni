0557c35174e97ec44c3bde2ad0d0d0eaeaf26d09
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-11-14 15:48:16 +0200

    New tasks

--- Fundamentals/Tasks/03 Data Types and Variables - Lab/03-10.latinLetters.js
+++ Fundamentals/Tasks/03 Data Types and Variables - Lab/03-10.latinLetters.js
@@ -1,16 +1,16 @@
-function latinLetters(n){
+function latinLetters(n) {
 
-    let a='';
-    let b='';
-    let c='';
+    let a = '';
+    let b = '';
+    let c = '';
     let result = ''
-    for(let s1 = 97; s1<97+Number(n); s1++){
-        for (let s2 = 97; s2<97+Number(n); s2++){
-            for (let s3 = 97; s3<97+Number(n); s3++){
-                a=String.fromCharCode(s1)
-                b=String.fromCharCode(s2)
-                c=String.fromCharCode(s3)
-                
+    for (let s1 = 97; s1 < 97 + Number(n); s1++) {
+        for (let s2 = 97; s2 < 97 + Number(n); s2++) {
+            for (let s3 = 97; s3 < 97 + Number(n); s3++) {
+                a = String.fromCharCode(s1)
+                b = String.fromCharCode(s2)
+                c = String.fromCharCode(s3)
+
                 console.log(`${a}${b}${c}`);
             }
         }
