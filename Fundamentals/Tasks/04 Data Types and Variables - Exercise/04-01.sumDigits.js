53dc9368456fe83dcb8e9e09d954a4bfb0c24fb6
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-11-15 11:48:31 +0200

    updates

--- Fundamentals/Tasks/04 Data Types and Variables - Exercise/04-01.sumDigits.js
+++ Fundamentals/Tasks/04 Data Types and Variables - Exercise/04-01.sumDigits.js
@@ -1,10 +1,12 @@
 function sumDigits(num) {
-    
+
     let numAsString = String(num);
     let sum = 0
-    for (let i = 0; i<numAsString.length; i++){
+
+    for (let i = 0; i < numAsString.length; i++) {
         sum += Number(numAsString[i])
     }
+
     console.log(sum);
 }
 sumDigits(245678)
