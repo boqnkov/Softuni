caaf9770a9a71512052fcb72c7cbb77922cc61a4
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-11-17 14:20:07 +0200

    update

--- Fundamentals/Tasks/04 Data Types and Variables - Exercise/04-10.spiceMustFlow.js
+++ Fundamentals/Tasks/04 Data Types and Variables - Exercise/04-10.spiceMustFlow.js
@@ -2,28 +2,18 @@ function spiceMustFlow(currYield) {
 
     let yield = 0
     let day = 0
+
     while (currYield >= 100) {
         day++
 
-        if (currYield < 100) {
-            brake
-        }
+        yield += currYield - 26
 
-
-        yield += currYield
-       
-        if (yield>26){
-        yield -= 26
-        } else {
-            yield-=currYield
-        }
         currYield -= 10
     }
-    if (yield>26){
+
+    if (yield >= 26) {
         yield -= 26
-        } else {
-            yield-=currYield
-        }
+    }
 
     console.log(day);
     console.log(yield);
