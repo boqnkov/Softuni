b6a941e23ce82d18062ce4328f6a9a0c41fe6848
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-11-25 19:15:19 +0200

    New tasks added

--- Fundamentals/Tasks/05 Arrays - Lab/05-06.evenAndOddSubtr.js
+++ Fundamentals/Tasks/05 Arrays - Lab/05-06.evenAndOddSubtr.js
@@ -1,15 +1,18 @@
 function subtraction(input) {
+
     let evenSum = 0;
     let oddSum = 0;
 
-    for(let curElement of input){
-        if(curElement%2 ==0){
-            evenSum+=curElement
+    for (let curElement of input) {
+        if (curElement % 2 == 0) {
+            evenSum += curElement
         } else {
-            oddSum+=curElement
+            oddSum += curElement
         }
     }
     let result = evenSum - oddSum
-    console.log(result);
+    console.log (result);
+
+    
 }
-subtraction([1,2,3,4,5,6])
+subtraction([1, 2, 3, 4, 5, 6])
---
+++ Fundamentals/Tasks/05 Arrays - Lab/05-07.equalArrays.js
@@ -0,0 +1,19 @@
+function equalArrays(input1, input2) {
+
+    let sum = 0;
+
+    for (let i = 0; i<input1.length; i++){
+        if (input1[i] == input2[i]){
+            sum+=Number(input1[i])
+        } else {
+            console.log(`Arrays are not identical. Found difference at ${i} index`);
+            return
+        }
+    }
+
+    console.log(`Arrays are identical. Sum: ${sum}`);
+
+}
+equalArrays
+    (['10', '200', '30'],
+    ['10', '20', '30'])
