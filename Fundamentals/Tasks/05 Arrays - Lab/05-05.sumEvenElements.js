2e6e41feff3a3dd2bc93318a72851054e0251dce
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-11-25 18:48:26 +0200

    New tasks

--- Fundamentals/Tasks/05 Arrays - Lab/05-05.sumEvenElements.js
+++ Fundamentals/Tasks/05 Arrays - Lab/05-05.sumEvenElements.js
@@ -1,11 +1,11 @@
 function sumEvenNums(input) {
 
     let result = 0
-    for(let curElement of input){
-        if (curElement % 2 ==0){
-            result+=Number(curElement)
+    for (let curElement of input) {
+        if (curElement % 2 == 0) {
+            result += Number(curElement)
         }
     }
     console.log(result);
 }
-sumEvenNums(['1','2','3','4','5','6'] )
+sumEvenNums(['1', '2', '3', '4', '5', '6'])
---
+++ Fundamentals/Tasks/05 Arrays - Lab/05-06.evenAndOddSubtr.js
@@ -0,0 +1,15 @@
+function subtraction(input) {
+    let evenSum = 0;
+    let oddSum = 0;
+
+    for(let curElement of input){
+        if(curElement%2 ==0){
+            evenSum+=curElement
+        } else {
+            oddSum+=curElement
+        }
+    }
+    let result = evenSum - oddSum
+    console.log(result);
+}
+subtraction([1,2,3,4,5,6])
