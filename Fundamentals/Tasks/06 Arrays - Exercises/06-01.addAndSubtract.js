997cb761f582e5a0c24767558febceb1bd6a8c3e
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-11-30 11:24:25 +0200

    editedTask

--- Fundamentals/Tasks/06 Arrays - Exercises/06-01.addAndSubtract.js
+++ Fundamentals/Tasks/06 Arrays - Exercises/06-01.addAndSubtract.js
@@ -1,19 +1,21 @@
 function addAndSubtract(arr) {
+
     let originalArrSum = 0;
     let newArrSum = 0;
 
-    for (let i=0; i<arr.length; i++){
-        originalArrSum+=arr[i]
-        
-        if (arr[i]%2==0){
-        arr[i]=arr[i]+i
+    for (let i = 0; i < arr.length; i++) {
+        originalArrSum += arr[i]
+
+        if (arr[i] % 2 == 0) {
+            arr[i] = arr[i] + i
         } else {
-            arr[i]=arr[i]-i
+            arr[i] = arr[i] - i
         }
-        newArrSum+=arr[i]
+        newArrSum += arr[i]
     }
     console.log(arr);
     console.log(originalArrSum);
     console.log(newArrSum);
+
 }
 addAndSubtract([5, 15, 23, 56, 35])
