a3030596dfd26491a496357da647cc5b4ab7a598
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-01 10:22:52 +0200

    Edited task

---
+++ Fundamentals/Tasks/06 Arrays - Exercises/06-06.equalSums.js
@@ -0,0 +1,20 @@
+function equalSums(arr) {
+    
+    for(let i = 0; i<arr.length; i++){
+        let leftSum=0
+        let rightSum=0
+        for (let j=0; j<i; j++){
+            leftSum+=arr[j]
+        }
+        for (let y = i+1; y<arr.length; y++){
+            rightSum+=arr[y]
+        }
+        if (leftSum==rightSum){
+            console.log(i);
+            return
+        }
+    }
+    console.log('no');
+
+}
+equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4])
