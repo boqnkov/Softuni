75e6c102c48f773ccca51163911802bc1a729783
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-12 11:57:03 +0200

    Create 07-02.myMath.js

---
+++ Fundamentals/Tasks/07 Functions - Lab/07-02.myMath.js
@@ -0,0 +1,12 @@
+function myMath(x,y) {
+    let result = 1;
+    for(let i=0; i<y; i++){
+        result = multiplyTwoNums(result , x)
+    }
+
+    function multiplyTwoNums(a,b){
+        return a*b
+    }
+    return result
+}
+myMath(2,8)
