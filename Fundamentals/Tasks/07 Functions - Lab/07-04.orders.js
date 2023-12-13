7d6f797d73ff3b3cef722e54076a251dffde2c22
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-13 15:31:46 +0200

    Create 07-04.orders.js

---
+++ Fundamentals/Tasks/07 Functions - Lab/07-04.orders.js
@@ -0,0 +1,13 @@
+function orders(product, quantity) {
+
+    let result = 0
+    switch (product) {
+        case 'coffee': result = quantity * 1.5; break;
+        case 'coke': result = quantity * 1.4; break;
+        case 'water': result = quantity * 1; break;
+        case 'snacks': result = quantity * 2; break;
+
+    }
+    console.log(result.toFixed(2));
+}
+orders("water", 5)
