ef652044d94fa8a78564e331d69982f548fa3095
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-13 13:31:34 +0200

    Create 07-05.simpleCalculator.js

---
+++ Fundamentals/Tasks/07 Functions - Lab/07-05.simpleCalculator.js
@@ -0,0 +1,19 @@
+function calculator(numOne, numTwo, operator) {
+    let multiplyResult = (a, b) => a * b
+    let divideResult = (a, b) => a / b
+    let addResult = (a, b) => a + b
+    let subtractResult = (a, b) => a - b
+    let result = 0
+    switch (operator) {
+        case 'multiply': result = multiplyResult(numOne, numTwo); break;
+        case 'divide': result =divideResult(numOne, numTwo); break
+        case 'add': result =addResult(numOne, numTwo); break
+        case 'subtract': result =subtractResult(numOne, numTwo); break
+    }
+    console.log(result);
+    
+}
+calculator(5,
+    5,
+    'multiply'
+)
