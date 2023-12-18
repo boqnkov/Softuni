2f0801adc37ad270d62122b09940f828ae2db961
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-18 14:50:30 +0200

    Update 08-02.addAndSubtract.js

--- Fundamentals/Tasks/08 Functions - Exercises/08-02.addAndSubtract.js
+++ Fundamentals/Tasks/08 Functions - Exercises/08-02.addAndSubtract.js
@@ -1,21 +1,13 @@
-function addAndSubtract(a,b,c) {
-    
-    let sum = (a,b) => a+b
-    let subtract = (a,b) => a-b
+function addAndSubtract(a, b, c) {
+
+    let sum = (a, b) => a + b
+    let subtract = (a, b) => a - b
 
 
-    let result1 = sum(a,b);
+    let result1 = sum(a, b);
     let result2 = subtract(result1, c)
     console.log(result2);
 
-    
 
-    // function sum(a,b) {
-    //     return a+b
-    // }
-
-    // function subtract(a,b) {
-    //     return a-b
-    // }
 }
-addAndSubtract(1,2,3)
+addAndSubtract(1, 2, 3)
