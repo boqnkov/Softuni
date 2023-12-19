0da791b9e771b0c81e1d97612ed319652a0125e4
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-19 10:01:23 +0200

    Update 08-03.charactersInRange.js

--- Fundamentals/Tasks/08 Functions - Exercises/08-03.charactersInRange.js
+++ Fundamentals/Tasks/08 Functions - Exercises/08-03.charactersInRange.js
@@ -4,17 +4,13 @@ function charactersInRange(char1, char2) {
     let b = char2.charCodeAt(0);
     let result = ''
 
-    if (a > b) {
-        for (let i = b+1; i < a; i++) {
+    let minCode = Math.min(a,b)
+    let maxCode = Math.max(a,b)
+    
+        for (let i = minCode+1; i < maxCode; i++) {
             result += String.fromCharCode(i) + ' '
         }
-    } else if (b > a) {
-        for (let i = a+1; i < b; i++) {
-            result += String.fromCharCode(i) + ' '
-        }
-    } else {
-        result = String.fromCharCode(i)
-    }
+    
 
     console.log(result);
 }
