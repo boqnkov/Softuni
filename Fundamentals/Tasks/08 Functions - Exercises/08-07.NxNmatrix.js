4ba1de98bb12940d52c8a2bd01db9fa26fe5436b
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-20 15:49:06 +0200

    Update 08-07.NxNmatrix.js

--- Fundamentals/Tasks/08 Functions - Exercises/08-07.NxNmatrix.js
+++ Fundamentals/Tasks/08 Functions - Exercises/08-07.NxNmatrix.js
@@ -1,8 +1,9 @@
 function matrix(num) {
-    for(let i=0; i<num; i++){
+
+    for (let i = 0; i < num; i++) {
         let curRow = `${num} `.repeat(num)
         console.log(curRow);
     }
-    
+
 }
 matrix(3)
