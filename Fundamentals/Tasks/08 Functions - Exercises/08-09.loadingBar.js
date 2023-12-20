349c823b46fd5a0a0900ff4be65902d8103467d0
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-20 16:38:21 +0200

    Create 08-09.loadingBar.js

---
+++ Fundamentals/Tasks/08 Functions - Exercises/08-09.loadingBar.js
@@ -0,0 +1,25 @@
+function loadingBar(num) {
+    let percent = `${num}%`
+    let forPrint = loading(num)
+    if (percent == 100){
+        console.log(`100% Complete!`);
+        console.log(`[%%%%%%%%%%]`);
+    } else {
+        console.log(`${percent} [${forPrint}]`);
+        console.log(`Still loading...`);
+    }
+
+    function loading(bar){
+        let result = []
+        for (let i=1; i<=10; i++){
+            if (i<=bar/10){
+                result.push('%')
+            } else {
+                result.push('.')
+            }
+        }
+        result = result.join('')
+        return result
+    }
+}
+loadingBar(60)
