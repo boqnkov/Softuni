ac4bc8ad38a0b4b944ba7bd81ec410abd01b0d50
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-20 15:16:47 +0200

    Create 08-06.passValidator.js

---
+++ Fundamentals/Tasks/08 Functions - Exercises/08-06.passValidator.js
@@ -0,0 +1,52 @@
+function passvalidator(password) {
+    
+    let checklengthVar = checkLength(password);
+    let checkCharVar = checkChar(password);
+    let checkDigitsCountVar = checkDigitsCount(password);
+
+
+    if (checklengthVar && checkCharVar && checkDigitsCountVar){
+        console.log(`Password is valid`);
+    }
+
+
+    function checkLength(password) {
+       if (password.length<6 || password.length>10){
+        console.log(`Password must be between 6 and 10 characters`);
+        return false
+       } else {
+        return true
+       }
+       
+    }
+
+    function checkChar(password) {
+        
+        for (let curSym of password){
+            let asciiCurSym = curSym.charCodeAt()
+            if (!((asciiCurSym>48 && asciiCurSym<57) || (asciiCurSym>65 && asciiCurSym<90) || (asciiCurSym>97 && asciiCurSym<122)) ){
+                console.log(`Password must consist only of letters and digits`);
+                return false
+            }
+        }
+        return true
+    }
+
+    function checkDigitsCount(password) {
+        let digitCounter = 0
+        for (let curChar of password){
+            let code = curChar.charCodeAt()
+            if ((code >= 48 && code<=57)){
+                digitCounter++
+            } 
+        }
+        
+        if (digitCounter<2){
+            console.log(`Password must have at least 2 digits`);
+            return false
+        } else {
+            return true
+        }
+    }
+}
+passvalidator('logI32n%')
