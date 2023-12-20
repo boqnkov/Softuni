cd34b1de8e15737e6b09afe9e47ff958c733125b
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-20 16:47:22 +0200

    Update 08-08.perfectNum.js

--- Fundamentals/Tasks/08 Functions - Exercises/08-08.perfectNum.js
+++ Fundamentals/Tasks/08 Functions - Exercises/08-08.perfectNum.js
@@ -1,12 +1,14 @@
 function perfectNum(num) {
 
     let sum = 0
+
     for (let divisor = 1; divisor < num; divisor++) {
 
         if (num % divisor == 0) {
             sum += divisor
         }
     }
+
     if (num == sum) {
         console.log(`We have a perfect number!`);
     } else {
