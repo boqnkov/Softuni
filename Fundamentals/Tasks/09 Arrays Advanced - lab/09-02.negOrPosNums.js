459a38b68fa537513572969078d4c7946d9cea64
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-22 12:11:45 +0200

    Create 09-02.negOrPosNums.js

---
+++ Fundamentals/Tasks/09 Arrays Advanced - lab/09-02.negOrPosNums.js
@@ -0,0 +1,16 @@
+function negOrPosNums(arr) {
+    
+    let result = []
+    
+    for (let currEl of arr){
+        
+        if (Number(currEl)>=0){
+            result.push(Number(currEl))
+        } else {
+            result.unshift(Number(currEl))
+        }
+    }
+
+    console.log(result.join('\n'));
+}
+negOrPosNums(['7', '-2', '8', '9'])
