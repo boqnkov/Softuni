c3b48a3cb4c880a6ef76eb60b1bb1cc66a4d970e
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-22 12:11:34 +0200

    Update 09-01.sumFirstAndLast.js

--- Fundamentals/Tasks/09 Arrays Advanced - lab/09-01.sumFirstAndLast.js
+++ Fundamentals/Tasks/09 Arrays Advanced - lab/09-01.sumFirstAndLast.js
@@ -1,8 +1,9 @@
 function sumFisrsAndLast(arr) {
+
     let firstElem = Number(arr.shift());
     let lastElem = Number(arr.pop());
 
-    let sum = firstElem+lastElem
+    let sum = firstElem + lastElem
     console.log(sum);
 }
 sumFisrsAndLast(['20', '30', '40'])
