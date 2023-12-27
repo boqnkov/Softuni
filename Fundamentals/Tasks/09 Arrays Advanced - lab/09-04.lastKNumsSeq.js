59dff84af3c885bbe50d417834696616e735ae31
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-27 16:43:20 +0200

    Create 09-04.lastKNumsSeq.js

---
+++ Fundamentals/Tasks/09 Arrays Advanced - lab/09-04.lastKNumsSeq.js
@@ -0,0 +1,15 @@
+function lastKNumsSeq(n, k) {
+    let result = [1];
+    for (let i = 1; i < n; i++) {
+        let start = Math.max(0,i-k)
+        let end  = i-1
+        let sum = 0
+        for (let j= start; j<=end; j++){
+            sum+=result[j]
+        }
+        result[i] = sum
+    
+    }
+    console.log(result);
+}
+lastKNumsSeq(6, 3)
