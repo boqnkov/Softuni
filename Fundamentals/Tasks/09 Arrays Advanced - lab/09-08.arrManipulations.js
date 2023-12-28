e9d2b8e9c6fe76ad58a083e679d97fca866671db
Boyan Kovchazliev <boqn.ko@gmail.com>
2023-12-28 17:27:01 +0200

    Create 09-08.arrManipulations.js

---
+++ Fundamentals/Tasks/09 Arrays Advanced - lab/09-08.arrManipulations.js
@@ -0,0 +1,14 @@
+function arrManipulations(input) {
+    
+    let arr = input
+    .shift()
+    .split(' ')
+    
+    console.log(arr);
+}
+arrManipulations(['4 19 2 53 6 43',
+'Add 3',
+'Remove 2',
+'RemoveAt 1',
+'Insert 8 3']
+)
