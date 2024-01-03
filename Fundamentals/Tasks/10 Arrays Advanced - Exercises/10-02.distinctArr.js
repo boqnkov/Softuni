54e909f9db548bd75989151862bad88b921d44b0
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-03 15:42:45 +0200

    Create 10-02.distinctArr.js

---
+++ Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-02.distinctArr.js
@@ -0,0 +1,11 @@
+function distinctArr(arr) {
+    let result = [];
+
+    for (let num of arr){
+        if (!result.includes(num)){
+            result.push(num)
+        }
+    }
+    console.log(result.join(' '));
+}
+distinctArr([7, 8, 9, 7, 2, 3, 4, 1, 2])
