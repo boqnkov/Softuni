f53fd8c8da8c03869686bbb92e7a0e316bb17061
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-08 16:48:16 +0200

    Update 10-06.bobmNums.js

--- Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-06.bobmNums.js
+++ Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-06.bobmNums.js
@@ -6,14 +6,14 @@ function bombNums(arr1, arr2) {
     for (let curNum of arr1) {
         let indexOfCurNum = arr1.indexOf(curNum)
         if (curNum == specialNum) {
-            
-            arr1.splice((Math.max(0, indexOfCurNum - neighbours)), ((neighbours * 2) + 1),0)
+
+            arr1.splice((Math.max(0, indexOfCurNum - neighbours)), ((neighbours * 2) + 1), 0)
         }
     }
 
     let sum = 0
     arr1.forEach(num => sum += num)
-    console.log(arr1);
+    // console.log(arr1);
     console.log(sum);
 }
 bombNums([1, 1, 2, 1, 1, 1, 2, 1, 1, 1],
