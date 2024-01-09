e51116c2856df36f0d9ad83f84ca3f6ef1575925
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-09 13:23:53 +0200

    Update 10-arrManipulator.js

--- Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-arrManipulator.js
+++ Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-arrManipulator.js
@@ -1,51 +1,65 @@
-function manipulator(nums,commands) {
-    
-    for (let command of commands){
+function manipulator(nums, commands) {
+
+    for (let command of commands) {
         let tokens = command.split(' ')
         let action = tokens[0]
-        if (action == 'add'){
+
+        if (action == 'add') {
             let idx = Number(tokens[1]);
             let num = Number(tokens[2]);
-            nums.splice(idx,0,num);
+
+            nums.splice(idx, 0, num);
 
         } else if (action == 'addMany') {
             let idx = Number(tokens[1]);
             let numsToAdd = tokens.slice(2,)
-            for (let num of numsToAdd){
-                nums.splice(idx,0,Number(num));
+
+            for (let num of numsToAdd) {
+                nums.splice(idx, 0, Number(num));
                 idx++
             }
-        } else if (action == 'contains'){
+
+        } else if (action == 'contains') {
             let num = Number(tokens[1])
             let containsResult = nums.indexOf(num)
+
             console.log(containsResult);
-        } else if (action == 'remove'){
+
+        } else if (action == 'remove') {
             let idx = Number(tokens[1])
-            nums.splice(idx,1)
-        } else if (action == 'shift'){
+
+            nums.splice(idx, 1)
+
+        } else if (action == 'shift') {
             let positions = Number(tokens[1]);
-            for (let i = 0; i<positions; i++){
+
+            for (let i = 0; i < positions; i++) {
                 let takenNum = nums.shift();
                 nums.push(takenNum)
             }
-        } else if (action == 'sumPairs'){
-            let newArr=[]
-            for (i=0; i<nums.length/2; i+=2){
-                let pushedNum = nums[i]+nums[i+1]
-                newArr.push(pushedNum)
+
+        } else if (action == 'sumPairs') {
+            let newArr = []
+
+            for (i = 0; i < nums.length / 2; i += 2) {
+                let pushedNum = nums[i] + nums[i + 1]
+
+                if (i + 1 < nums.length) {
+                    newArr.push(pushedNum)
+                } else {
+                    newArr.push(nums[i])
+                }
             }
             nums = newArr
-        } else if (action == 'print'){
-        console.log(nums);
+        } else if (action == 'print') {
+            console.log(`[ ${nums.join(', ')}]`);
         }
-    
     }
-
 }
 // manipulator([1, 2, 4, 5, 6, 7],
 //     ['add 1 8', 'contains 1', 'contains 3', 'print']
 //     )
 
-    manipulator([1, 2, 3, 4, 5],
-        ['addMany 5 9 8 7 6 5', 'contains 15', 'remove 3', 'shift 1', 'print']
-        )
+manipulator([1, 2, 3, 4, 5],
+    ['addMany 5 9 8 7 6 5', 'contains 15', 'remove 3', 'shift 1', 'print']
+)
