a15a68bcbeba6805f216febff5c0524022beee1d
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-10 17:19:40 +0200

    Update 10-09.gladiatorInventory.js

--- Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-09.gladiatorInventory.js
+++ Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-09.gladiatorInventory.js
@@ -3,39 +3,40 @@ function gladiatorInventory(arr) {
     let inventory = inventoryAsStr.split(' ')
 
 
-    for (let command of arr){
+    for (let command of arr) {
         let tokens = command.split(' ')
         let action = tokens[0]
         let equipment = tokens[1];
 
-        if (action == 'Buy'){
-            if (!inventory.includes(equipment)){
+        if (action == 'Buy') {
+            if (!inventory.includes(equipment)) {
                 inventory.push(equipment)
             }
-        } else if (action == 'Trash'){
-            if(inventory.includes(equipment)){
-            let idx = inventory.indexOf(equipment);
-            inventory.splice(idx,1)
+        } else if (action == 'Trash') {
+            if (inventory.includes(equipment)) {
+                let idx = inventory.indexOf(equipment);
+                inventory.splice(idx, 1)
             }
-        } else if (action == 'Repair'){
-            if (inventory.includes(equipment)){
+        } else if (action == 'Repair') {
+            if (inventory.includes(equipment)) {
                 let idx = inventory.indexOf(equipment);
                 let repairedEq = inventory[idx];
-                inventory.splice(idx,1);
-                inventory.push(repairedEq)}
-        } else if (action == 'Upgrade'){
-                let eqAsArr = equipment.split('-')
-                let eq = eqAsArr[0] ;
-                let upgrade = eqAsArr[1];
-                if (inventory.includes(eq)){
-                    let idx= inventory.indexOf(eq);
-                    let addedEl = `${eq}:${upgrade}`
-                    inventory.splice(idx+1,0,addedEl)
-                }
+                inventory.splice(idx, 1);
+                inventory.push(repairedEq)
+            }
+        } else if (action == 'Upgrade') {
+            let eqAsArr = equipment.split('-')
+            let eq = eqAsArr[0];
+            let upgrade = eqAsArr[1];
+            if (inventory.includes(eq)) {
+                let idx = inventory.indexOf(eq);
+                let addedEl = `${eq}:${upgrade}`
+                inventory.splice(idx + 1, 0, addedEl)
             }
         }
-        console.log(inventory.join(' '));
     }
+    console.log(inventory.join(' '));
+}
 
 
 // gladiatorInventory(['SWORD Shield Spear',
@@ -46,7 +47,7 @@ function gladiatorInventory(arr) {
 // )
 
 gladiatorInventory(['SWORD Shield Spear',
-'Trash Bow',
-'Repair Shield',
-'Upgrade Helmet-V']
+    'Trash Bow',
+    'Repair Shield',
+    'Upgrade Helmet-V']
 )
