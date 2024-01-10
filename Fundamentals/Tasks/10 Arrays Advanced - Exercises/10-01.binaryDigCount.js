df7a2b5b0cbae18c59a8965f3af6979531a8c424
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-10 17:19:11 +0200

    Create 10-01.binaryDigCount.js

---
+++ Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-01.binaryDigCount.js
@@ -0,0 +1,11 @@
+function binaryCount(int, b){
+let numBin = int.toString(2)
+let counter = 0
+for (let curB of numBin){
+    if (curB == b){
+        counter++
+    }
+}
+console.log(counter);
+}
+binaryCount(20,0)
