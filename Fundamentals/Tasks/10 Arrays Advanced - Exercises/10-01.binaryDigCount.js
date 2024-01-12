ab6274825060cb7b9ab404b8fbf6eee602bc987f
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-12 19:47:37 +0200

    Update 10-01.binaryDigCount.js

--- Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-01.binaryDigCount.js
+++ Fundamentals/Tasks/10 Arrays Advanced - Exercises/10-01.binaryDigCount.js
@@ -1,11 +1,12 @@
-function binaryCount(int, b){
-let numBin = int.toString(2)
-let counter = 0
-for (let curB of numBin){
-    if (curB == b){
-        counter++
+function binaryCount(int, b) {
+
+    let numBin = int.toString(2)
+    let counter = 0
+    for (let curB of numBin) {
+        if (curB == b) {
+            counter++
+        }
     }
+    console.log(counter);
 }
-console.log(counter);
-}
-binaryCount(20,0)
+binaryCount(20, 0)
