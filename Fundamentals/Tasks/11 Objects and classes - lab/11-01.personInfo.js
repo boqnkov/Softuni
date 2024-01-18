6d5cc9b2faa91c16e12a1e361daa627682eebe8f
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-18 14:59:46 +0200

    Create 11-01.personInfo.js

---
+++ Fundamentals/Tasks/11 Objects and classes - lab/11-01.personInfo.js
@@ -0,0 +1,11 @@
+function personInfo(firstName, lastName, age) {
+    let object = {firstName, lastName, age};
+    return object
+    
+}
+let result = personInfo("Peter", 
+"Pan",
+"20"
+)
+
+console.log(result);
