2edbecd3576414f58320e40c07439b6b6869c0f1
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-19 15:09:26 +0200

    Create 11-03.convertToObject(JSON).js

---
+++ Fundamentals/Tasks/11 Objects and classes - lab/11-03.convertToObject(JSON).js
@@ -0,0 +1,11 @@
+function convertToObject(input){
+
+    let person = JSON.parse(input)
+    let entries= Object.entries(person)
+    for (let [key, value] of entries){
+        console.log(`${key}: ${value}`);
+    }
+
+    
+}
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}')
