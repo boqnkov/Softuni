c7d4ff58b79033177e13b71a17f7a548a365d3c6
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-24 16:06:10 +0200

    Create 12-demo.js

---
+++ Fundamentals/Tasks/12 Object and classes- Exercises/12-demo.js
@@ -0,0 +1,40 @@
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.ages = age
+//     }
+
+//     sayHello() {
+//         console.log(`${this.name} says Hello`);
+//     }
+// }
+// let myPerson = new Person('Ivan', 22)
+// console.log(myPerson)
+// myPerson.sayHello()
+
+function solve(arr) {
+
+    class Cat {
+        name;
+        age;
+        constructor(name, age) {
+            this.name = name;
+            this.age = age
+        }
+
+        meow(){
+            console.log(`${this.name} ${this.age} says Meow`);
+        }
+    }
+
+    for (let cats of arr){
+        let [name, age] = cats.split(' ')
+        let cat = new Cat(name,age)
+    
+        cat.meow()
+    }
+}
+
+
+
+solve(['Mellow 2', 'Tom 5'])
