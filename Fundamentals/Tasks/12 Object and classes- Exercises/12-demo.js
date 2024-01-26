1a32556a76e9f2f9427716a28863038c5ef67e71
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-26 13:58:56 +0200

    Update 12-demo.js

--- Fundamentals/Tasks/12 Object and classes- Exercises/12-demo.js
+++ Fundamentals/Tasks/12 Object and classes- Exercises/12-demo.js
@@ -1,40 +1,38 @@
-// class Person {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.ages = age
-//     }
-
-//     sayHello() {
-//         console.log(`${this.name} says Hello`);
-//     }
-// }
-// let myPerson = new Person('Ivan', 22)
-// console.log(myPerson)
-// myPerson.sayHello()
-
-function solve(arr) {
-
-    class Cat {
-        name;
-        age;
-        constructor(name, age) {
-            this.name = name;
-            this.age = age
-        }
-
-        meow(){
-            console.log(`${this.name} ${this.age} says Meow`);
-        }
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.ages = age
     }
 
-    for (let cats of arr){
-        let [name, age] = cats.split(' ')
-        let cat = new Cat(name,age)
-    
-        cat.meow()
+    sayHello() {
+        console.log(`${this.name} says Hello`);
     }
 }
+let myPerson = new Person('Ivan', 22)
+console.log(myPerson)
+myPerson.sayHello()
 
+// function solve(arr) {
 
+//     class Cat {
+//         name;
+//         age;
+//         constructor(name, age) {
+//             this.name = name;
+//             this.age = age
+//         }
 
-solve(['Mellow 2', 'Tom 5'])
+//         meow(){
+//             console.log(`${this.name} ${this.age} says Meow`);
+//         }
+//     }
+
+//     for (let cats of arr){
+//         let [name, age] = cats.split(' ')
+//         let cat = new Cat(name,age)
+    
+//         cat.meow()
+//     }
+// }
+
+// solve(['Mellow 2', 'Tom 5'])
