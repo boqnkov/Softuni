0210ca55cd536193e34c238db914afb40370f314
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-29 16:37:35 +0200

    Update 12-07.Vehicle.js

--- Fundamentals/Tasks/12 Object and classes- Exercises/12-07.Vehicle.js
+++ Fundamentals/Tasks/12 Object and classes- Exercises/12-07.Vehicle.js
@@ -1,26 +1,43 @@
-class Vehicle {
+// class Vehicle {
 
-    constructor(type,model,parts, fuel){
+//     constructor(type,model,parts, fuel){
+//         this.type = type;
+//         this.model = model;
+//         this.parts = parts;
+//         this.fuel = fuel;
+//         this.parts.quality = this.parts.engine * this.parts.power
+
+//     }
+
+//     drive(fuelLoss){
+//         this.fuel = this.fuel - fuelLoss
+//     }
+// }
+
+
+// let parts = { engine: 6, power: 100 };
+// let vehicle = new Vehicle('a', 'b', parts, 200);
+// vehicle.drive(100);
+// console.log(vehicle.fuel);
+// console.log(vehicle.parts.quality);
+
+class Vehicle {
+    constructor(type, model, parts, fuel){
         this.type = type;
         this.model = model;
         this.parts = parts;
-        this.fuel = fuel;
-        this.parts.quality = this.parts.engine * this.parts.power
-
+        this.fuel = fuel
+        this.parts['quality'] = parts.engine * parts.power
     }
 
     drive(fuelLoss){
-        this.fuel = this.fuel - fuelLoss
+        this.fuel-=fuelLoss
     }
 }
-
 
 let parts = { engine: 6, power: 100 };
 let vehicle = new Vehicle('a', 'b', parts, 200);
 vehicle.drive(100);
 console.log(vehicle.fuel);
 console.log(vehicle.parts.quality);
-
-
-
 
