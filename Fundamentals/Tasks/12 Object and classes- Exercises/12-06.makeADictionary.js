67047a8077bf0ab1c3d1eb8f62659fb19316cb5b
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-01-31 16:00:26 +0200

    Update 12-06.makeADictionary.js

--- Fundamentals/Tasks/12 Object and classes- Exercises/12-06.makeADictionary.js
+++ Fundamentals/Tasks/12 Object and classes- Exercises/12-06.makeADictionary.js
@@ -51,8 +51,8 @@ function dictionary(arr) {
 
 
     dictionary([
-        '{"Coffee":"....."}',
         '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+        '{"Coffee":"....."}',
         '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
         '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
         '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
