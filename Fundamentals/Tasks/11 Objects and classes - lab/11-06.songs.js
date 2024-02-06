8d4136de07fdf5d0534c6c5f0218e3fb1aae28b6
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-02-06 14:46:49 +0200

    Create 11-06.songs.js

---
+++ Fundamentals/Tasks/11 Objects and classes - lab/11-06.songs.js
@@ -0,0 +1,38 @@
+function songs(arr) {
+
+    let songsNum = arr.shift()
+    let typeList = arr.pop()
+    let songsArr = []
+
+    class Song {
+        typeList
+        name
+        time
+        constructor(typeList, name, time) {
+            this.typeList = typeList;
+            this.name = name;
+            this.time = time;
+        }
+
+    }
+
+    for (let i = 0; i < songsNum; i++) {
+        let [typeList, name, time] = arr[i].split('_');
+        let song = new Song(typeList, name, time);
+        songsArr.push(song)
+    }
+
+    if (typeList == 'all'){
+        songsArr.forEach((i) => console.log(i.name))
+    } else {
+        let filtered = songsArr.filter((i) => i.typeList == typeList);
+        filtered.forEach((i) => console.log(i.name))
+    }
+
+}
+songs([3,
+    'favourite_DownTown_3:14',
+    'favourite_Kiss_4:16',
+    'favourite_Smooth Criminal_4:01',
+    'favourite']
+)
