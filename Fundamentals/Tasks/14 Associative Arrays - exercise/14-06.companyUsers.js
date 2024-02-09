d25636fd5a06b7601858b320068110d6c711bf94
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-02-09 10:17:51 +0200

    Create 14-06.companyUsers.js

---
+++ Fundamentals/Tasks/14 Associative Arrays - exercise/14-06.companyUsers.js
@@ -0,0 +1,35 @@
+function companyUsers(arr) {
+    
+    let info = {}
+    
+    for (let command of arr ){
+        let [company, id] = command.split(' -> ');
+        if (!(company in info)) {
+            info[company] = [id]
+        } else {
+            if (!info[company].includes(id)){
+            info[company].push(id)
+            }
+        }
+    }
+
+    let entries = Object.entries(info).sort((a,b) => a[0].localeCompare(b[0]));
+
+    console.log(entries);
+
+    for (let [company, id] of entries){
+        console.log(company);
+        id.forEach(id => console.log(`--${id}`));
+    }
+
+
+
+
+}
+companyUsers([
+    'SoftUni -> AA12345',
+    'SoftUni -> BB12345',
+    'Microsoft -> CC12345',
+    'HP -> BB12345'
+    ]
+    )
