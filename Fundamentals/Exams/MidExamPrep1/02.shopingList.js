8e79e3bd56e589f5ac440e6690c050fe5b88042e
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-02-16 15:30:37 +0200

    Update 02.shopingList.js

--- Fundamentals/Exams/MidExamPrep1/02.shopingList.js
+++ Fundamentals/Exams/MidExamPrep1/02.shopingList.js
@@ -1,8 +1,10 @@
 function shoppingList(arr) {
     let list = arr.shift().split('!')
+    
     for (let command of arr) {
         let curCommand = command.split(',')
         let commandAsArr = curCommand[0].split(' ')
+        
         if (commandAsArr[0] == 'Urgent') {
             if (!(list.includes(commandAsArr[1]))) {
                 list.unshift(commandAsArr[1])
@@ -15,12 +17,14 @@ function shoppingList(arr) {
         } else if (commandAsArr[0] == 'Correct') {
             if (list.includes(commandAsArr[1])) {
                 let index = list.indexOf(commandAsArr[1])
-                list.splice(index, 1, commandAsArr[2])
+                list[index] = commandAsArr[2]
             }
         } else if (commandAsArr[0] == 'Rearrange') {
-            if (list.includes(commandAsArr[1])) {
-                let found = list.find((element) => element == commandAsArr[1])
-                list.push(found)
+            let removeIndex = list.indexOf(commandAsArr[1])
+            if (removeIndex != -1) {
+                let element = list[removeIndex]
+                list.splice(removeIndex,1);
+                list.push(element)
             }
         }
     }
