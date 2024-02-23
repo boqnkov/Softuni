5bc5e8ba65bd19569f60611cb336b582cb98ac12
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-02-23 16:16:28 +0200

    Create 14-02.oddOccurrences.js

---
+++ Fundamentals/Tasks/14 Associative Arrays - exercise/14-02.oddOccurrences.js
@@ -0,0 +1,11 @@
+function oddOccurrences(string) {
+
+    let array = string.split(' ');
+    
+    for (let element of array) {
+        
+    }
+
+
+}
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
