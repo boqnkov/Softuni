bf8b8371e6270b218da4895fac8186521fad234f
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-02-26 16:58:33 +0200

    Update 14-05.cardGame.js

--- Fundamentals/Tasks/14 Associative Arrays - exercise/14-05.cardGame.js
+++ Fundamentals/Tasks/14 Associative Arrays - exercise/14-05.cardGame.js
@@ -5,46 +5,58 @@ function cardGame(arr) {
     for (let command of arr) {
         let [name, cards] = command.split(': ');
         let powerOfCards = cards.split(', ')
-        let setOfCards = new Set(powerOfCards)
-        console.log(setOfCards);
 
-        for (let curCard of setOfCards) {
-            let curCardAsArr = curCard.split('')
-            let type = curCardAsArr.pop()
-            let powerAsSybmol = curCardAsArr.join('')
-            let power = 0
-            let typePower = 0
-            if (type == 'S') {
-                typePower = 4
-            } else if (type == 'H') {
-                typePower = 3
-            } else if (type == 'D') {
-                typePower = 2
-            } else if (type == 'C') {
-                typePower = 1
-            }
-
-            if (powerAsSybmol == 'J') {
-                power = 11
-            } else if (powerAsSybmol == 'Q') {
-                power = 12
-            } else if (powerAsSybmol == 'K') {
-                power = 13
-            } else if (powerAsSybmol == 'A') {
-                power = 14
-            } else {
-                power = Number(powerAsSybmol)
-            }
-            let powerOfCurCard = typePower * power
-
-            if (name in finalResult) {
-                finalResult[name] += powerOfCurCard
-
-            } else {
-                finalResult[name] = powerOfCurCard
+        if (!(name in finalResult)) {
+            finalResult[name] = powerOfCards
+        } else {
+            for (let curCard of powerOfCards) {
+                finalResult[name].push(curCard)
             }
         }
     }
+
+    for (let cards of Object.keys(finalResult)){
+        let uniqueCards = new Set(cards)
+        finalResult = uniqueCards
+        
+    }
+
+
+
+    for (let curCard of powerOfCards) {
+        let curCardAsArr = curCard.split('')
+        let type = curCardAsArr.pop()
+        let powerAsSybmol = curCardAsArr.join('')
+        let power = 0
+        let typePower = 0
+        if (type == 'S') {
+            typePower = 4
+        } else if (type == 'H') {
+            typePower = 3
+        } else if (type == 'D') {
+            typePower = 2
+        } else if (type == 'C') {
+            typePower = 1
+        }
+
+        if (powerAsSybmol == 'J') {
+            power = 11
+        } else if (powerAsSybmol == 'Q') {
+            power = 12
+        } else if (powerAsSybmol == 'K') {
+            power = 13
+        } else if (powerAsSybmol == 'A') {
+            power = 14
+        } else {
+            power = Number(powerAsSybmol)
+        }
+        let powerOfCurCard = typePower * power
+
+        finalResult[name] += powerOfCurCard
+    }
+
+
+
 
     let entries = Object.entries(finalResult)
     for (let [name, result] of entries) {
