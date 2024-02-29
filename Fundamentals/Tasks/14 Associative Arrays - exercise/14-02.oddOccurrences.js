eb08a0b6459d20c8fbbee80a9497f835c1478653
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-02-29 17:28:18 +0200

    Update 14-02.oddOccurrences.js

--- Fundamentals/Tasks/14 Associative Arrays - exercise/14-02.oddOccurrences.js
+++ Fundamentals/Tasks/14 Associative Arrays - exercise/14-02.oddOccurrences.js
@@ -1,10 +1,28 @@
-function oddOccurrences(string) {
+function oddOccurrences(input) {
+    let string = input.toLowerCase();
+    let arr = string.split(' ');
 
-    let array = string.split(' ');
+    let elements = {};
+
     
-    for (let element of array) {
-        
+    for (let element of arr) {
+        if (element in elements) {
+            elements[element] += 1
+        } else {
+            elements[element] = 1
+        }
     }
+
+    let entries = Object.entries(elements)
+    let output =[];
+
+    for (let [element, count] of entries){
+        if (count % 2 !== 0) {
+            output.push(element)
+        }
+    }
+    console.log(output.join(' '));
+
 
 
 }
