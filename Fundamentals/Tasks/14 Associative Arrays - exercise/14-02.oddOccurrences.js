6cd34d1f8faf5c49dafbe965782a5a0f7ce54a9c
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-03-03 23:46:35 +0200

    Update 14-02.oddOccurrences.js

--- Fundamentals/Tasks/14 Associative Arrays - exercise/14-02.oddOccurrences.js
+++ Fundamentals/Tasks/14 Associative Arrays - exercise/14-02.oddOccurrences.js
@@ -2,28 +2,23 @@ function oddOccurrences(input) {
     let string = input.toLowerCase();
     let arr = string.split(' ');
 
-    let elements = {};
+    let object = {};
 
-    
     for (let element of arr) {
-        if (element in elements) {
-            elements[element] += 1
+        if (!(element in object)) {
+            object[element] = 1
         } else {
-            elements[element] = 1
+            object[element]++
         }
     }
 
-    let entries = Object.entries(elements)
-    let output =[];
+    let entries = Object.entries(object)
+        .sort((a, b) => b[1] - a[1])
+        .filter(a => a[1] % 2 != 0)
 
-    for (let [element, count] of entries){
-        if (count % 2 !== 0) {
-            output.push(element)
-        }
-    }
-    console.log(output.join(' '));
-
-
+    let result = []
+    entries.forEach(el => result.push(el[0]))
+    console.log(result.join(' '));
 
 }
 oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
