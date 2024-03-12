ce6966eb9e59700578097436a553f4468ba08c03
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-03-12 15:48:17 +0200

    Create 16-07.cutAndReverse.js

---
+++ Fundamentals/Tasks/16 Text Processing - exercise/16-07.cutAndReverse.js
@@ -0,0 +1,17 @@
+function cutAndReverse(input) {
+    
+    let firstHalf = '';
+    let secondHalf = '';
+
+    for (let i= (input.length)/2-1; i>=0; i--){
+        firstHalf+=input[i]
+    }
+
+    for (let i= (input.length)-1; i>=(input.length)/2; i--){
+        secondHalf+=input[i]
+    }
+
+    console.log(firstHalf);
+    console.log(secondHalf);
+}
+cutAndReverse('tluciffiDsIsihTgnizamAoSsIsihT')
