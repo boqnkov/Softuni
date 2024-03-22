a98d7a5752360a602da872e943c7501add6282b6
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-03-22 16:53:21 +0200

    Update 18-06.emailExtract.js

--- Fundamentals/Tasks/18 RegEx - Exercises/18-06.emailExtract.js
+++ Fundamentals/Tasks/18 RegEx - Exercises/18-06.emailExtract.js
@@ -1,7 +1,7 @@
 function emailExtract(input){
-let pattern = /\b[^\.\-_]\w*[-\._]*\w*[^\.\-_]@[A-Za-z\-]+\.[A-Za-z\-]*\.?[A-Za-z]*\b/gm
+let pattern = /( |^)([A-Za-z0-9]+[\w\.\-]*)@([A-za-z]+\-?[A-Za-z]+)(\.[A-Za-z]+\-?[A-Za-z]){1,2}/g
 
 let match = input.match(pattern)
 match.forEach(match => console.log(match))
 }
-emailExtract('Many users @ SoftUni confuse email addresses. We @ Softuni.BG provide high-quality training @ home or @ class. –- steve.parker@softuni.de.')
+emailExtract('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.')
