5bd54b4c9375326a9669a4a823b26fced89fa23f
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-03-26 14:30:15 +0200

    Create 04.destinationMapper.js

---
+++ Fundamentals/Exams/FinalExamPrep2/04.destinationMapper.js
@@ -0,0 +1,19 @@
+function destMapper(input) {
+    let pattern = /([=\/])(?<dest>[A-Z][A-Za-z]{2,})\1/g
+
+    let matches = pattern.exec(input)
+    let destinations = []
+    while (matches){
+        destinations.push(matches.groups.dest)
+        matches = pattern.exec(input)
+    }
+
+    let points = 0
+
+    for (let destination of destinations){
+        points+=destination.length
+    }
+    console.log(`Destinations: ${destinations.join(', ')}`);
+    console.log(`Travel Points: ${points}`);
+}
+destMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=")
