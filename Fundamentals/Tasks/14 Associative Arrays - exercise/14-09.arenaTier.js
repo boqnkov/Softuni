97aaab51bb4945c132703f264e80911836601abf
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-03-28 14:20:12 +0200

    Update 14-09.arenaTier.js

--- Fundamentals/Tasks/14 Associative Arrays - exercise/14-09.arenaTier.js
+++ Fundamentals/Tasks/14 Associative Arrays - exercise/14-09.arenaTier.js
@@ -5,6 +5,7 @@ function arenaTier(input) {
 
     while (command != "Ave Cesar") {
         let [firstEl, secondEl, skill] = command.split(' -> ');
+        skill = Number(skill)
         if (skill) {
             let gladiator = firstEl
             let technique = secondEl
@@ -18,59 +19,89 @@ function arenaTier(input) {
                 }
             } else {
                 gladiatorPool[gladiator] = { [technique]: skill }
+                
             }
         } else {
             let [gladiator1, gladiator2] = firstEl.split(' vs ')
             if ((gladiator1 in gladiatorPool) && (gladiator2 in gladiatorPool)) {
-                let technArr = []
-                let entries = Object.values(gladiatorPool)
-                for (let techniques of entries) {
-                    let array = Object.keys(techniques)
-                    technArr.push(...array)
-                }
-                let set = new Set(technArr)
+                
+                let keysGlad1 = Object.keys(gladiatorPool[gladiator1])
+                let keysGlad2 = Object.keys(gladiatorPool[gladiator2])
+                keysGlad1.push(...keysGlad2)
 
-                if (technArr.length == set.size) {
-                    let totalScore1 = 0;
-                    let totalScore2 = 0;
+                let set = new Set(keysGlad1)
+                let setSize = set.size
 
-                    let values1 = Object.values(gladiatorPool[gladiator1])
-                    values1.forEach(x => totalScore1 += Number(x))
+                if (keysGlad1.length != set.size){
+                    let totalPointsGlad1Arr = Object.values(gladiatorPool[gladiator1])
+                    let totalPointsGlad2Arr = Object.values(gladiatorPool[gladiator2])
+                    let totalPointsGlad1 = totalPointsGlad1Arr.reduce((acc, curVal) => acc+curVal)
+                    let totalPointsGlad2 = totalPointsGlad2Arr.reduce((acc, curVal) => acc+curVal)
 
-
-                    let values2 = Object.values(gladiatorPool[gladiator2])
-                    values2.forEach(x => totalScore2 += Number(x))
-
-                    totalScore1 > totalScore2 ? delete gladiatorPool[gladiator2] : delete gladiatorPool[gladiator1]
+                    if (totalPointsGlad1 > totalPointsGlad2){
+                        delete gladiatorPool[gladiator2]
+                    } else if (totalPointsGlad1 < totalPointsGlad2){
+                        delete gladiatorPool[gladiator1]
+                    }
                 }
             }
         }
         command = input.shift()
     }
 
-
-    let skillPoints = {}
-
     for (let gladiator in gladiatorPool) {
 
-        skillPoints[gladiator] = 0
+        gladiatorPool[gladiator]['totalSkill'] = 0
         let value = Object.values(gladiatorPool[gladiator])
-        value.forEach(skill => skillPoints[gladiator] += Number(skill))
+        value.forEach(skill => gladiatorPool[gladiator]['totalSkill'] += Number(skill))
     }
-    let entries = Object.entries(skillPoints);
-    let sorted = entries.sort((a,b) => (b[1] - a[1]) || (a[0].localeCompare(b[0])))
     
-
-    let sortedTechniques = {}
+    let sorted = Object.entries(gladiatorPool).sort((a,b)=>b[1].totalSkill - a[1].totalSkill || a[0].localeCompare(b[0]))
     
+    for (let gladiator of sorted){
+        let name = gladiator[0];
+        let skill = gladiator[1].totalSkill
+        console.log(`${name}: ${skill} skill`);
 
+        let sortedTechs = Object.entries(gladiatorPool[name]).sort((a,b)=>b[1] - a[1] || a[0].localeCompare(b[0]))
+    
+        for (let tech of sortedTechs){
+            let technique = tech[0]
+            let skill = tech[1]
+
+            if (technique == 'totalSkill'){
+                continue;
+            }
+            console.log(`- ${technique} <!> ${skill}`);
+        }
+    }
 }
 arenaTier([
-    'Peter -> BattleCry -> 400',
-    'Alex -> PowerPunch -> 300',
-    'Stefan -> Duck -> 200',
-    'Stefan -> Tiger -> 250',
+    'Peter -> Duck -> 400',
+    'Julius -> Shield -> 150',
+    'Gladius -> Heal -> 200',
+    'Gladius -> Support -> 250',
+    'Gladius -> Shield -> 250',
+    'Peter vs Gladius',
+    'Gladius vs Julius',
+    'Gladius vs Maximilian',
     'Ave Cesar'
     ]
+    )
+
+
+
+
+
+
+
+
+// arenaTier([
+//     'Peter -> BattleCry -> 400',
+//     'Alex -> PowerPunch -> 300',
+//     'Stefan -> Duck -> 200',
+//     'Stefan -> Tiger -> 250',
+//     'Ave Cesar'
+//     ]
     
-)
+// )
