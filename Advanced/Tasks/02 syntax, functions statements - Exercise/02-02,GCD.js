c3f5c58ff43bf547aae12c73ea6ac6614f91ee1a
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-05 12:26:10 +0300

    Create 02-02,GCD.js

---
+++ Advanced/Tasks/02 syntax, functions statements - Exercise/02-02,GCD.js
@@ -0,0 +1,12 @@
+function gcd(num1, num2) {
+
+    while (num1 !== num2){
+        if (num1>num2){
+            num1-=num2
+        } else {
+            num2-=num1
+        }
+    }
+    console.log(num1);
+}
+gcd(15, 5)
