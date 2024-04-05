c827facd0f2e22e056f5225b386e0bb09196cd6e
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-05 12:45:21 +0300

    Update 10.agregateElements.js

--- Advanced/Tasks/01 syntax, functions statements - Lab/10.agregateElements.js
+++ Advanced/Tasks/01 syntax, functions statements - Lab/10.agregateElements.js
@@ -1,16 +1,15 @@
 function agregateEls(arr) {
+
     let sum = 0
-    arr.forEach(element => {
-        sum+=element
-    });
+    arr.forEach(element => sum += element);
     console.log(sum);
 
     let inverse = 0;
-    arr.forEach((el) => inverse+=1/el)
+    arr.forEach((el) => inverse += 1 / el)
     console.log(inverse);
 
     let string = '';
-    arr.forEach((el) => string+=el);
+    arr.forEach((el) => string += el);
     console.log(string);
 }
 agregateEls([1, 2, 3])
