51e127ae5de0694508dc0081573e27b573dc6d93
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-09 15:50:31 +0300

    Update 02-06.roadRadar.js

--- Advanced/Tasks/02 syntax, functions statements - Exercise/02-06.roadRadar.js
+++ Advanced/Tasks/02 syntax, functions statements - Exercise/02-06.roadRadar.js
@@ -1,109 +1,86 @@
 function roadRadar(speed, type) {
-    // let limit = 0
-    // let status = ''
-    // let diff = 0
-    // switch (type) {
-    //     case "motorway":
-    //         limit = 130
-    //         diff = Math.abs(speed - limit)
+    let limit = 0
+    let status = ''
+    let diff = 0
+    switch (type) {
+        case "motorway":
+            limit = 130
+            diff = Math.abs(speed - limit)
 
-    //         if (speed <= limit) {
-    //             console.log(`Driving ${speed} km/h in a ${limit} zone`);
-    //         } else {
-    //             if (diff > 40) {
-    //                 status = 'reckless driving'
-    //             } else if (diff > 20) {
-    //                 status = 'excessive speeding'
-    //             } else {
-    //                 status = 'speeding'
-    //             }
+            if (speed <= limit) {
+                console.log(`Driving ${speed} km/h in a ${limit} zone`);
+            } else {
+                if (diff > 40) {
+                    status = 'reckless driving'
+                } else if (diff > 20) {
+                    status = 'excessive speeding'
+                } else {
+                    status = 'speeding'
+                }
 
-    //             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
-    //         }
-    //         break;
-    //     case "interstate":
-    //         limit = 90
-    //         diff = Math.abs(speed - limit)
+                console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
+            }
+            break;
+        case "interstate":
+            limit = 90
+            diff = Math.abs(speed - limit)
 
-    //         if (speed <= limit) {
-    //             console.log(`Driving ${speed} km/h in a ${limit} zone`);
-    //         } else {
-    //             if (diff > 40) {
-    //                 status = 'reckless driving'
-    //             } else if (diff > 20) {
-    //                 status = 'excessive speeding'
-    //             } else {
-    //                 status = 'speeding'
-    //             }
+            if (speed <= limit) {
+                console.log(`Driving ${speed} km/h in a ${limit} zone`);
+            } else {
+                if (diff > 40) {
+                    status = 'reckless driving'
+                } else if (diff > 20) {
+                    status = 'excessive speeding'
+                } else {
+                    status = 'speeding'
+                }
 
-    //             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
-    //         }
-    //         break;
+                console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
+            }
+            break;
 
-    //         case "city":
-    //         limit = 50
-    //         diff = Math.abs(speed - limit)
+            case "city":
+            limit = 50
+            diff = Math.abs(speed - limit)
 
-    //         if (speed <= limit) {
-    //             console.log(`Driving ${speed} km/h in a ${limit} zone`);
-    //         } else {
-    //             if (diff > 40) {
-    //                 status = 'reckless driving'
-    //             } else if (diff > 20) {
-    //                 status = 'excessive speeding'
-    //             } else {
-    //                 status = 'speeding'
-    //             }
+            if (speed <= limit) {
+                console.log(`Driving ${speed} km/h in a ${limit} zone`);
+            } else {
+                if (diff > 40) {
+                    status = 'reckless driving'
+                } else if (diff > 20) {
+                    status = 'excessive speeding'
+                } else {
+                    status = 'speeding'
+                }
 
-    //             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
-    //         }
-    //         break;
+                console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
+            }
+            break;
 
-    //         case "residential":
-    //         limit = 20
-    //         diff = Math.abs(speed - limit)
+            case "residential":
+            limit = 20
+            diff = Math.abs(speed - limit)
 
-    //         if (speed <= limit) {
-    //             console.log(`Driving ${speed} km/h in a ${limit} zone`);
-    //         } else {
-    //             if (diff > 40) {
-    //                 status = 'reckless driving'
-    //             } else if (diff > 20) {
-    //                 status = 'excessive speeding'
-    //             } else {
-    //                 status = 'speeding'
-    //             }
+            if (speed <= limit) {
+                console.log(`Driving ${speed} km/h in a ${limit} zone`);
+            } else {
+                if (diff > 40) {
+                    status = 'reckless driving'
+                } else if (diff > 20) {
+                    status = 'excessive speeding'
+                } else {
+                    status = 'speeding'
+                }
 
-    //             console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
-    //         }
-    //         break;
-    //     default:
-    //         break;
-    // }
-
-    const limitsDictionary = {
-        motorway: 130,
-        interstate: 90,
-        city: 50,
-        residential: 20
+                console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
+            }
+            break;
+        default:
+            break;
     }
 
-    let limit = limitsDictionary[type]
 
-    if (speed <= limit){
-        console.log(`Driving ${speed} km/h in a ${limit} zone`);
-        return
-    } else {
-        let status = '';
-        let diff = speed - limit;
-        if (diff > 40){
-            status = 'reckless driving'
-        } else if (diff > 20){
-            status = 'excessive speeding'
-        } else if (diff > 0){
-            status = 'speeding'
-        }
-        console.log(`The speed is ${diff} km/h faster than the allowed speed of ${limit} - ${status}`);
-    }
 }
 roadRadar(120, 'interstate')
