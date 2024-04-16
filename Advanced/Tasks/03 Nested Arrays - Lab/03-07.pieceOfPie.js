cf9d99b8e56a9ee217b65fa17ffd1324c56811b8
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-16 15:54:12 +0300

    Create 03-07.pieceOfPie.js

---
+++ Advanced/Tasks/03 Nested Arrays - Lab/03-07.pieceOfPie.js
@@ -0,0 +1,19 @@
+function pieceOfPie(...params) {
+    let arr = params[0];
+    const firstFlavour = params[1];
+    const secondFlavour = params[2];
+
+    const idxFirst = arr.indexOf(firstFlavour);
+    const idxSecond = arr.indexOf(secondFlavour);
+
+    const result = arr.slice(idxFirst, idxSecond + 1);
+    return result
+}
+pieceOfPie(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'
+)
