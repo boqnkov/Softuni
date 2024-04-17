ccbc2641916bc0c64a41a9d1963929d303b1bb60
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-17 16:18:00 +0300

    Create 03-11.equalNeighbours.js

---
+++ Advanced/Tasks/03 Nested Arrays - Lab/03-11.equalNeighbours.js
@@ -0,0 +1,27 @@
+function equalNeighbours(matrix) {
+    let counter = 0
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            let char = matrix[i][j]
+            if (char === matrix[i][j + 1]) {
+                counter++
+            }
+            if (i !== matrix.length - 1) {
+                if (char === matrix[i + 1][j]) {
+                    counter++
+                }
+            }
+        }
+    }
+    console.log(counter);
+}
+equalNeighbours([['test', 'yes', 'yo', 'ho'],
+                ['well', 'done', 'yo', '6'],
+                ['not', 'done', 'yet', 'yet']]
+)
+
+// equalNeighbours([['2', '3', '4', '7', '0'],
+//                 ['4', '0', '5', '3', '4'],
+//                 ['2', '8', '5', '4', '2'],
+//                 ['9', '2', '7', '5', '5']]
+// )
