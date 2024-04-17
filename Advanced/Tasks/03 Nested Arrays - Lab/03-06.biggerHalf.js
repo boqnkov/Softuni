d7eed1ef44b9fb09961f94ba75db6f06954dff7a
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-17 16:17:23 +0300

    Update 03-06.biggerHalf.js

--- Advanced/Tasks/03 Nested Arrays - Lab/03-06.biggerHalf.js
+++ Advanced/Tasks/03 Nested Arrays - Lab/03-06.biggerHalf.js
@@ -1,9 +1,9 @@
 function biggerHalf(input) {
-    
-    let sorted = input.sort((a,b)=> a - b);
-    let halfIndex = Math.ceil(input.length/2);
+
+    let sorted = input.sort((a, b) => a - b);
+    let halfIndex = Math.ceil(input.length / 2);
     let result = sorted.slice(-halfIndex);
     return result
 
-}
+} 
 biggerHalf([3, 19, 14, 7, 2, 19, 6])
