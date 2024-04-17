bf0f001fc4990a3ac961494ba30283de54718496
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-17 16:17:44 +0300

    Update 03-09.biggestEl.js

--- Advanced/Tasks/03 Nested Arrays - Lab/03-09.biggestEl.js
+++ Advanced/Tasks/03 Nested Arrays - Lab/03-09.biggestEl.js
@@ -1,11 +1,14 @@
 function biggestEl(input) {
     let bigArr = [];
-    for (let arr of input){
+    for (let arr of input) {
         bigArr.push(...arr)
     }
-    console.log(bigArr);
+
+    const sorted = bigArr.sort((a, b) => b - a)
+    const result = sorted[0];
+    return result
 }
 biggestEl([[3, 5, 7, 12],
-    [-1, 4, 33, 2],
-    [8, 3, 0, 4]]
-   )
+[-1, 4, 33, 2],
+[8, 3, 0, 4]]
+)
