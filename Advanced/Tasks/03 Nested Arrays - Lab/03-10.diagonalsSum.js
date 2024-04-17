e75c2ce622145ecff3bd04247ffa253a35ba783a
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-17 16:17:54 +0300

    Create 03-10.diagonalsSum.js

---
+++ Advanced/Tasks/03 Nested Arrays - Lab/03-10.diagonalsSum.js
@@ -0,0 +1,14 @@
+function diagonalsSum(matrix) {
+let main = 0;
+let second =0;
+
+for (let i = 0; i<matrix.length; i++){
+    main+=matrix[i][i]
+    second+= matrix[i][matrix.length-1-i]
+}
+console.log(main, second);
+}
+diagonalsSum([[3, 5, 17],
+            [-1, 7, 14],
+            [1, -8, 89]]
+)
