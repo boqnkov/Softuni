b138601903125935bb2b3c0432a73b7b991873ca
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-17 16:17:28 +0300

    Update 03-07.pieceOfPie.js

--- Advanced/Tasks/03 Nested Arrays - Lab/03-07.pieceOfPie.js
+++ Advanced/Tasks/03 Nested Arrays - Lab/03-07.pieceOfPie.js
@@ -10,10 +10,10 @@ function pieceOfPie(...params) {
     return result
 }
 pieceOfPie(['Pumpkin Pie',
-'Key Lime Pie',
-'Cherry Pie',
-'Lemon Meringue Pie',
-'Sugar Cream Pie'],
-'Key Lime Pie',
-'Lemon Meringue Pie'
+    'Key Lime Pie',
+    'Cherry Pie',
+    'Lemon Meringue Pie',
+    'Sugar Cream Pie'],
+    'Key Lime Pie',
+    'Lemon Meringue Pie'
 )
