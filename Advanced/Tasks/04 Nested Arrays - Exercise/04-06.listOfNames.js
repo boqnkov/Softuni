51283c43f66dac2e3128fda904027b2cca76f70b
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-18 14:57:36 +0300

    Create 04-06.listOfNames.js

---
+++ Advanced/Tasks/04 Nested Arrays - Exercise/04-06.listOfNames.js
@@ -0,0 +1,8 @@
+function listOfNames(arr) {
+    let sorted = arr.sort((a,b) => a.localeCompare(b))
+    sorted.forEach((x, i) => {
+        console.log(`${i + 1}. ${x}`)
+    })
+    }
+
+listOfNames(["John", "Bob", "Christina", "Ema"])
