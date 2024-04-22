af2689ed1e9bc87cfa1a0e42e00703dd57d4f5c8
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-22 14:10:20 +0300

    Update 04-09.magicMatrix.js

--- Advanced/Tasks/04 Nested Arrays - Exercise/04-09.magicMatrix.js
+++ Advanced/Tasks/04 Nested Arrays - Exercise/04-09.magicMatrix.js
@@ -3,15 +3,15 @@ function magicMatrix(arr) {
     let sum = arr[0].reduce((acc, value) => acc + value)
     for (let i = 0; i < arr.length; i++) {
 
-        let curSum = arr[i].reduce((acc, value) => acc + value)
-        if (sum !== curSum) {
-            return false
+        let sumRow =0 ;
+        let sumCol = 0;
+
+        
+        for (let j = 0; j < arr.length; j++) {
+            sumRow += arr[i][j]
+            sumCol += arr[j][i]
         }
-        curSum = 0
-        for (let j = 0; j < arr[i].length; j++) {
-            curSum += arr[i][j]
-        }
-        if (sum !== curSum) {
+        if (sum !== sumRow || sum !== sumCol) {
             return false
         }
 
