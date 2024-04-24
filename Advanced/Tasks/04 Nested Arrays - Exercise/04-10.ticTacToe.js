991da093332a2deead0df15d2baa1040a79c01a7
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-04-24 15:47:30 +0300

    Update 04-10.ticTacToe.js

--- Advanced/Tasks/04 Nested Arrays - Exercise/04-10.ticTacToe.js
+++ Advanced/Tasks/04 Nested Arrays - Exercise/04-10.ticTacToe.js
@@ -9,59 +9,59 @@ function ticTacToe(arr) {
         const [row, col] = line.split(' ').map(e => Number(e));
 
 
-        if (dashboard[row][col] !== false){
+        if (dashboard[row][col] !== false) {
             console.log(`This place is already taken. Please choose another!`);
             continue
-        } 
-            
+        }
+
         dashboard[row][col] = player;
 
-        for (let i = 0; i<3; i++){
+        for (let i = 0; i < 3; i++) {
             if (
                 (dashboard[i][0] === player &&
-                dashboard[i][1] === player &&
-                dashboard[i][2] === player) ||
+                    dashboard[i][1] === player &&
+                    dashboard[i][2] === player) ||
                 (dashboard[0][i] === player &&
-                dashboard[1][i] === player &&
-                dashboard[2][i] === player)
-            ){
+                    dashboard[1][i] === player &&
+                    dashboard[2][i] === player)
+            ) {
                 console.log(`Player ${player} wins!`);
                 printMatrix()
                 return;
-                
+
             }
 
             if (
                 (dashboard[0][0] === player &&
                     dashboard[1][1] === player &&
                     dashboard[2][2] === player) ||
-                    (dashboard[0][2] === player &&
+                (dashboard[0][2] === player &&
                     dashboard[1][1] === player &&
                     dashboard[2][0] === player)
-            ){
+            ) {
                 console.log(`Player ${player} wins!`);
                 printMatrix()
                 return;
             }
 
             let includesFalse = false;
-             for (let row = 0; row<dashboard.length; row++){
-                if (dashboard[row].includes(false)){
+            for (let row = 0; row < dashboard.length; row++) {
+                if (dashboard[row].includes(false)) {
                     includesFalse = true
                 }
-             }
-             if (!includesFalse){
+            }
+            if (!includesFalse) {
                 console.log(`The game ended! Nobody wins :(`);
-            printMatrix() ;
-            return;
+                printMatrix();
+                return;
 
             }
-            player = player === "X"? 'O':'X'
-        }  
+            player = player === "X" ? 'O' : 'X'
+        }
     }
 
     function printMatrix() {
-        for (let row = 0 ; row<dashboard.length; row ++){
+        for (let row = 0; row < dashboard.length; row++) {
             console.log(dashboard[row].join('\t'));
         }
     }
