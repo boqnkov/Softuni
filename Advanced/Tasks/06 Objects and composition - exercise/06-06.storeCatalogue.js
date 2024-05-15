1d96f2263dc0d5877298298c7a7c54555caf698d
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-05-15 16:17:47 +0300

    Create 06-06.storeCatalogue.js

---
+++ Advanced/Tasks/06 Objects and composition - exercise/06-06.storeCatalogue.js
@@ -0,0 +1,36 @@
+function storeCatalogue(input) {
+    
+    let result = [];
+
+    for (let line of input){
+        let [product, price] = line.split(' : ')
+        price = Number(price)
+        result.push({product, price})
+    }
+
+    let sorted = result.sort((a,b) => a.product.localeCompare(b.product))
+    console.log(sorted);
+
+    let symbol = '';
+
+    for (let line of sorted){
+        if (symbol !== line.product[0]){
+            console.log(line.product[0]);
+            symbol = line.product[0]
+        } 
+
+        console.log(`  ${line.product}: ${line.price}`);
+    }
+
+
+
+}
+storeCatalogue(['Appricot : 20.4',
+'Fridge : 1500',
+'TV : 1499',
+'Deodorant : 10',
+'Boiler : 300',
+'Apple : 1.25',
+'Anti-Bug Spray : 15',
+'T-Shirt : 10']
+)
