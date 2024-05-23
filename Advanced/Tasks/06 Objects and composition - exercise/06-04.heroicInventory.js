56056e7dbbe92b2ff2ea08121f5c92ac26a9ba01
Boyan Kovchazliev <boqn.ko@gmail.com>
2024-05-23 16:13:24 +0300

    Update 06-04.heroicInventory.js

--- Advanced/Tasks/06 Objects and composition - exercise/06-04.heroicInventory.js
+++ Advanced/Tasks/06 Objects and composition - exercise/06-04.heroicInventory.js
@@ -2,32 +2,20 @@ function heroicInventory(arr) {
     
     let result = [];
 
-//     for (let i = 0; i<arr.length; i++){
-//         let tokens = arr[i]
-
-//         let [name, level, items] = tokens.split(' / ')
-//         level = Number(level)
-//         items = items ? items.split(', ') : [];
+    for (let i = 0; i<arr.length; i++){
+        let tokens = arr[i]
+        if (tokens.trim() !== ''){
+        let [name, level, items] = tokens.split(' / ')
+        level = Number(level)
+        items = items ? items.split(', ') : [];
         
 
-//         result.push({name, level, items})
-//     }
+        result.push({name, level, items})
+        }
+    }
       
-// return JSON.stringify(result)
-
-arr.forEach(str => {
-    const obj = {};
-    const heroDataArr = str.split(" / ");
-    obj.name = heroDataArr[0];
-    obj.level = Number(heroDataArr[1])
-
-    const items = heroDataArr[2];
-    obj.items = items? items.split(", ") : [];
-    result.push(obj);
-
-});
-
 return JSON.stringify(result)
+
 
 }
 heroicInventory(['Isacc / 25 / Apple, GravityGun',
